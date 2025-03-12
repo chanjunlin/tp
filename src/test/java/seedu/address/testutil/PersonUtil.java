e2a096354f776f793f05e4d44ef66f10ffb16aa3
shayne <sleongxy@gmail.com>
2025-03-12 17:11:22 +0800

    fix more test cases
    left with 7 cases

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -53,6 +53,10 @@ public class PersonUtil {
         descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
         descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
+        descriptor.getBloodType().ifPresent(bloodType -> sb.append(PREFIX_BLOODTYPE)
+                .append(bloodType.bloodType).append(" "));
+        descriptor.getAppointment().ifPresent(appointment -> sb.append(PREFIX_APPOINTMENT)
+                .append(appointment.appointment).append(" "));
         if (descriptor.getTags().isPresent()) {
             Set<Tag> tags = descriptor.getTags().get();
             if (tags.isEmpty()) {
