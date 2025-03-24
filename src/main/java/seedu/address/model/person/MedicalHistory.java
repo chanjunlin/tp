906e49a8671ba5ab762f05262efe939633f5eca1
jon-seana <e1122894@u.nus.edu>
2025-03-24 12:30:38 +0800

    Add space function for tag

--- src/main/java/seedu/address/model/person/MedicalHistory.java
+++ src/main/java/seedu/address/model/person/MedicalHistory.java
@@ -8,7 +8,7 @@ import static java.util.Objects.requireNonNull;
 public class MedicalHistory {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Medical history should only contain alphanumeric characters and spaces, and it cannot be blank!";
+            "Medical history should only contain alphanumeric characters and spaces, and it should not be blank";
     public static final String VALIDATION_REGEX = "^[a-zA-Z0-9 ]+$";
 
     public final String medicalHistory;
