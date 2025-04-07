b7b8b2740aef006c4bdf263d7394f530b7f8a1d9
shayne <sleongxy@gmail.com>
2025-04-07 14:14:54 +0800

    change duplicate person checking

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -160,8 +160,10 @@ public class Person {
         }
 
         return otherPerson != null
-                && otherPerson.getName().equals(getName());
-    }
+                && otherPerson.getName().equals(getName())
+                && otherPerson.getPhone().equals(getPhone())
+                && otherPerson.getDateOfBirth().equals(getDateOfBirth());
+    }   
 
     /**
      * Returns true if the appointment is nurse and false otherwise.
