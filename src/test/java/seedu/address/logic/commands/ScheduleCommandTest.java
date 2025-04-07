80bd975e71026fb4c4cd77a9d2743c140c7c2fc4
shayne <sleongxy@gmail.com>
2025-04-07 17:25:32 +0800

    fix test case

--- src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -183,7 +184,9 @@ public class ScheduleCommandTest {
         CommandException exception = assertThrows(CommandException.class, () -> {
             invalidIndexCommand.execute(model);
         });
-        assertEquals(MESSAGE_INVALID_PERSON_DISPLAYED_INDEX,
+        String msg =  String.format(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX, 7);
+
+        assertEquals(msg,
                 exception.getMessage());
     }
 }
